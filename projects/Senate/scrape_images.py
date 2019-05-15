1bb960a92db5feab0a733665d627a0ce3478278d
thoppe <travis.hoppe@gmail.com>
2019-05-15 17:44:30 -0400

    Started parsing

--- projects/Senate/scrape_images.py
+++ projects/Senate/scrape_images.py
@@ -1,2 +1,15 @@
 import requests, bs4
 
+with open('List_of_members_of_the_United_States_Senate.html') as FIN:
+    raw = FIN.read()
+
+soup = bs4.BeautifulSoup(raw, 'lxml')
+
+for a in soup.find_all('a', {"class":"image"}):
+
+    name = a.parent.findNext('td').get_text().strip()
+
+    if not name:
+        continue
+
+    print(name, a['href'])
