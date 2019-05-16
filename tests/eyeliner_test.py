b4635a7897804626717a47063ead1f3256f0b715
thoppe <travis.hoppe@gmail.com>
2019-05-16 16:02:24 -0400

    Linting

--- tests/eyeliner_test.py
+++ tests/eyeliner_test.py
@@ -2,17 +2,20 @@ import pytest
 import pixelhouse as ph
 import greasepaint as gp
 
+
 @pytest.fixture
 def test_canvas():
     canvas = ph.load("tests/tessa1.jpg")
     return canvas
 
+
 def test_eyeliner(test_canvas):
     # This only tests if the image changed
     dst = gp.eyeliner(test_canvas)
-    assert( not (dst.img == test_canvas.img).all() )
+    assert not (dst.img == test_canvas.img).all()
+
 
 def test_eyeshadow(test_canvas):
     # This only tests if the image changed
-    dst = gp.eyeshadow(test_canvas, color='r')
-    assert( not (dst.img == test_canvas.img).all() )
+    dst = gp.eyeshadow(test_canvas, color="r")
+    assert not (dst.img == test_canvas.img).all()
